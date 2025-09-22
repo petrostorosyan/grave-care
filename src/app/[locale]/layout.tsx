635b26f7383739f54c9c8fc8dd689691ef3e33b5
Petros <petrostorosyan23@gmail.com>
2025-09-22 11:34:46 +0400

    bug fix

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,52 +1,54 @@
-import {NextIntlClientProvider, hasLocale} from 'next-intl';
-import {notFound} from 'next/navigation';
-import {routing} from '@/i18n/routing';
-import { setRequestLocale } from 'next-intl/server';
-import type { Metadata } from 'next';
+import { NextIntlClientProvider, hasLocale } from "next-intl";
+import { notFound } from "next/navigation";
+import { routing } from "@/i18n/routing";
+import { setRequestLocale } from "next-intl/server";
+import type { Metadata } from "next";
 import "../globals.css";
 import { Lora } from "next/font/google";
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-import { AntdRegistry } from '@ant-design/nextjs-registry';
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+import { Suspense } from "react";
 
 const lora = Lora({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
-  variable: "--font-lora", 
+  variable: "--font-lora",
 });
- 
+
 export function generateStaticParams() {
-  return routing.locales.map((locale) => ({locale}));
+  return routing.locales.map((locale) => ({ locale }));
 }
 
 export const metadata: Metadata = {
-  title: 'Grave Care',
-  description: 'Grave Care',
-}
- 
+  title: "Grave Care",
+  description: "Grave Care",
+};
+
 export default async function RootLayout({
   children,
-  params
+  params,
 }: {
   children: React.ReactNode;
-  params: Promise<{locale: string}>;
+  params: Promise<{ locale: string }>;
 }) {
-
-  const {locale} = await params;
+  const { locale } = await params;
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
 
   setRequestLocale(locale);
- 
+
   return (
     <html lang={locale}>
       <body className={`${lora.variable}`}>
         <NextIntlClientProvider>
           <AntdRegistry>
-         <Header />
-          {children}
-          <Footer />
+            <Suspense fallback={<div>Loading...</div>}>
+              <Header />
+            </Suspense>
+            {children}
+            <Footer />
           </AntdRegistry>
         </NextIntlClientProvider>
       </body>
