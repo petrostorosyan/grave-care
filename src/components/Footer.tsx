7868f13a607fba196ca730e1ea3845fe050b4aa3
Petros <petrostorosyan23@gmail.com>
2025-11-23 19:42:11 +0400

    bug fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,19 @@
 import { Divider } from "antd";
 import FooterMenu from "./FooterMenu";
 import FooterRights from "./FooterRights";
+import { Suspense } from "react";
 
 const Footer = () => {
   return (
-    <footer className="pt-[80px] pb-[25px] px-[50px] flex flex-col justify-start items-center max-[830px]:p-[50px] 
-      max-[430px]:p-[15px] max-[650px]:px-[50px] transition-all duration-500">
+    <footer
+      className="pt-[80px] pb-[25px] px-[50px] flex flex-col justify-start items-center max-[830px]:p-[50px] 
+      max-[430px]:p-[15px] max-[650px]:px-[50px] transition-all duration-500"
+    >
       <div className="w-[1100px] min-h-[100px] text-[18px] max-[1200px]:w-[100%]">
         <Divider className="border !border-[#313236]" />
-        <FooterMenu />
+        <Suspense fallback={<div></div>}>
+          <FooterMenu />
+        </Suspense>
         <Divider className="border !border-[#313236]" />
         <FooterRights />
       </div>
